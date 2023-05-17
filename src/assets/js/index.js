27dab0b4daf767cc4d81e65af1367b57cbb4aef0
Fezzopro <fezzopro@gmail.com>
2023-05-18 01:53:26 +0200

    Add preloaded scores

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,9 @@
 import '../css/style.css';
+import game from '../modules/game';
+import components from '../modules/components';
+
+components.submitScoreEvent();
+components.refreshList();
+game.getScores().then((scores) => {
+  components.RenderScoreList(scores.result);
+});
