0d862bfe65e36234c644753b991dbde86be8e186
Fezzopro <fezzopro@gmail.com>
2023-05-18 01:55:21 +0200

    Add a game module to the application

---
+++ src/assets/modules/game.js
@@ -0,0 +1,71 @@
+import storage from "./storage";
+
+class GameAPI {
+  constructor() {
+    this.baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+    this.headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    };
+
+    // Create and save the game if it's your first time playing
+    if (storage.readLocalStorage().length === 0) {
+      this.createGame()
+        .then(response => storage.saveToLocalStorage({ gameId: this.splitForGameId(response.result), }));
+    }
+  }
+
+  createGame = async () => {
+    try {
+      const response = await fetch(this.baseUrl, {
+        method: 'POST',
+        body: JSON.stringify({ name: storage.GAMENAME, }),
+        headers: this.headers,
+      });
+
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  splitForGameId = (string) => {
+    return string.split(' ')[3];
+  };
+
+  getGameId = () => {
+    return storage.readLocalStorage()[0].gameId;
+  };
+
+  saveScore = async (score) => {
+    try {
+      const response = await fetch(`${this.baseUrl}${this.getGameId()}/scores/`,
+        {
+          method: 'POST',
+          body: JSON.stringify(score),
+          headers: this.headers,
+        },
+      );
+
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  getScores = async () => {
+    try {
+      const response = await fetch(`${this.baseUrl}${this.getGameId()}/scores/`,
+        {
+          method: 'GET',
+        },
+      );
+
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  };
+}
+
+export default new GameAPI();
